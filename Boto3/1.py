105526df1fdf5c5e15ebe5edc36873b74004f165
adiyenuganti501 <yenuganti501@gmail.com>
2026-09-13 07:58:52 +0530

    13 sept

---
+++ Boto3/1.py
@@ -0,0 +1,10 @@
+import boto3
+
+sts = boto3.client("sts")
+
+response = sts.get_caller_identity()
+
+# print(response)
+print(f"User Id:{response["UserId"]}")
+print("Account:", response["Account"])
+print("ARN:", response["Arn"])
--- Functions/sep12.py
+++ Functions/sep12.py
@@ -5,3 +5,9 @@ print(list(val))
 print("-------------------------")
 val=list(map(lambda x: x*x, range(5)))
 print(val)
+
+li=[1,2,3,4,5,6,7,8]
+print(li[::-4])
+
+li = [10, 20, 30, 40, 50, 60, 70, 80]
+print(li[8:0:-2])
