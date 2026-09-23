07c3cf8767643df094aa5193c71756fbc1779e6a
adiyenuganti501 <yenuganti501@gmail.com>
2026-09-23 08:21:01 +0530

    restore

--- OOPS/Inharitance.py
+++ OOPS/Inharitance.py
@@ -6,12 +6,7 @@ class Car:
     def showdetals(self):
         print(f"Details are {self.name} {self.brand} and {self.year}")
 class Audi(Car):
-    def __init__(self,name,brand,year,color):
-        super().__init__(name,brand,year)
-        self.color=color
-    def showColor(self):
-        print("Color is", self.color)
+    pass
 
-a=Audi("Audi","501",2026,"Grey")
+a=Audi("Audi","501",2026)
 a.showdetals()
-a.showColor()
