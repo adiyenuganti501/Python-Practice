33c2639257623c9f6dd974456493f5fc9ce4a327
adiyenuganti501 <yenuganti501@gmail.com>
2026-09-24 13:01:12 +0530

    24 sept

---
+++ Loops/1.py
@@ -0,0 +1,34 @@
+# for i in range(1,10):
+#     print(f"Server-{i}")
+
+# for i in range(1,10,2):
+#     print(i)
+    
+# for i in range(10,0,-1):
+#     print(i)
+    
+# name = "Python"
+# for ch in name:
+#     print(ch)
+
+# ip = "192.168.1.10"
+# for i in ip:
+#     print(i)
+
+# servers = ["web-01", "db-01", "web-02", "db-02"]
+# for s in servers:
+#     if s.startswith("web"):
+#         print(s)
+
+server = {
+    "name": "web-01",
+    "ip": "10.0.1.10",
+    "status": "running"
+}
+
+for k,v in server.items():
+    print(k,v)
+for i in range(1,11):
+    if i==5:
+        break
+    print(i)
