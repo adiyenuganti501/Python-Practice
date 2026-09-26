9272c92378179ed98988412d26ab73a9c3758cd2
adiyenuganti501 <yenuganti501@gmail.com>
2026-09-26 09:13:12 +0530

    26

---
+++ 2. Variables & Data Types/Dictionary/1.py
@@ -0,0 +1,18 @@
+user = {
+    "username": "coder123",
+    "age": 25,
+    "is_active": True
+}
+
+for i,v in user.items():
+    print(i,v)
+print("===============")    
+    
+# Creating an empty dictionary
+empty_dict = {}   
+empty_dict["name"]= "Adi"
+empty_dict["age"]=30
+print(empty_dict)
+
+for i,v in empty_dict.items():
+    print(i,v)
---
+++ StringReverse/str.py
@@ -0,0 +1,6 @@
+name="Adinarayana"
+print(name[:0:-1])
+
+
+# given = input("Enter the Value::")
+# print(given[:1:-1])
